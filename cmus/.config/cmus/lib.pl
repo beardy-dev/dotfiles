12c6c38d517e296e88e2ad00a7b2dd6668cb1590
beardy-dev <andrewcameron.corsini@gmail.com>
2025-07-15 13:47:37 -0500

    reorganized gnu stow packages, clean up

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,189 @@
+/home/beardydev/Music/amies/Healing/01 Healing.m4a
+/home/beardydev/Music/amies/Forest Cabin/01 Forest Cabin.m4a
+/home/beardydev/Music/Axian/Wait for You/01 Wait for You.m4a
+/home/beardydev/Music/Blumen/Always There/01 Always There.m4a
+/home/beardydev/Music/Blumen/Plenitude/02 Calm Journey.m4a
+/home/beardydev/Music/Blumen/Plenitude/04 Fields.m4a
+/home/beardydev/Music/Blumen/Flat Spin/01 Flat Spin.m4a
+/home/beardydev/Music/Boyan/Blue Hour/01 Blue Hour.m4a
+/home/beardydev/Music/C4C/Don't Be Late/01 Don't Be Late.m4a
+/home/beardydev/Music/C4C/Lost Ballads EP/01 Old School Love.m4a
+/home/beardydev/Music/Closed on Sunday/animal crossing ~ new horizons lofi/01 animal crossing ~ new horizons lofi.m4a
+/home/beardydev/Music/Closed on Sunday/somewhere over the rainbow ~ lofi/01 somewhere over the rainbow ~ lofi.m4a
+/home/beardydev/Music/Closed on Sunday/dearly beloved ~ kingdom hearts lofi/01 dearly beloved ~ kingdom hearts lofi.m4a
+/home/beardydev/Music/Closed on Sunday/the avatar's love ~ lofi/01 the avatar's love ~ lofi.m4a
+/home/beardydev/Music/Closed on Sunday/married life ~ lofi/01 married life ~ lofi.m4a
+/home/beardydev/Music/Closed on Sunday/sweden ~ minecraft lofi/01 sweden ~ minecraft lofi.m4a
+/home/beardydev/Music/Closed on Sunday/the mandalorian ~ star wars lofi/01 the mandalorian ~ star wars lofi.m4a
+/home/beardydev/Music/Closed on Sunday/rey's theme ~ star wars lofi/01 rey's theme ~ star wars lofi.m4a
+/home/beardydev/Music/Closed on Sunday/imagination ~ lofi/01 imagination ~ lofi.m4a
+/home/beardydev/Music/Cloud Break/Bubbles/01 Bubbles.m4a
+/home/beardydev/Music/Datsunn/Patience/01 Patience.m4a
+/home/beardydev/Music/DLJ/The Place/01 The Place.m4a
+/home/beardydev/Music/DLJ/A Place I Know/01 A Place I Know.m4a
+/home/beardydev/Music/DLJ/Just You/01 Just You.m4a
+/home/beardydev/Music/DLJ/Dreaming/01 Dreaming.m4a
+/home/beardydev/Music/DLJ/Suspended/01 Suspended.m4a
+/home/beardydev/Music/DLJ/Sedimento/01 Sedimento.m4a
+/home/beardydev/Music/DLJ/Discovery/01 Discovery.m4a
+/home/beardydev/Music/DLJ/Madness/01 Madness.m4a
+/home/beardydev/Music/DLJ/Memphis/01 Memphis.m4a
+/home/beardydev/Music/DLJ/Crépuscule/01 Crépuscule.m4a
+/home/beardydev/Music/DLJ/EDAM/05 Feeba.m4a
+/home/beardydev/Music/DLJ/Monte Carle/03 Villa.m4a
+/home/beardydev/Music/DLJ/Sunset Drive/01 Sunset Drive.m4a
+/home/beardydev/Music/DLJ/Lost Path/01 Lost Path.m4a
+/home/beardydev/Music/DLJ/Room to Breathe/01 Room to Breathe.m4a
+/home/beardydev/Music/DLJ/Blue Lounge/01 Blue Lounge.m4a
+/home/beardydev/Music/Dosi/Out There/01 Out There.m4a
+/home/beardydev/Music/Dosi/Zéphyr/01 Zéphyr.m4a
+/home/beardydev/Music/Dosi/Destiny/01 Destiny.m4a
+/home/beardydev/Music/Dosi/Busy Bee/01 Azur.m4a
+/home/beardydev/Music/Dosi/Busy Bee/05 Amma.m4a
+/home/beardydev/Music/Dosi/Busy Bee/06 On the Moon.m4a
+/home/beardydev/Music/Dosi/Escape/01 Escape.m4a
+/home/beardydev/Music/Dosi/Riverside/01 Riverside.m4a
+/home/beardydev/Music/Dosi/Look Around/01 Look Around.m4a
+/home/beardydev/Music/Enough Cereals/Foresight/01 Foresight.m4a
+/home/beardydev/Music/Enough Cereals/Impulse/02 Impulse.m4a
+/home/beardydev/Music/Flandrs/Hidamari/01 Hidamari.m4a
+/home/beardydev/Music/FloFilz/Nightly Passage/01 Nightly Passage.m4a
+/home/beardydev/Music/FloFilz/Dim Down/04 Where Is The Love.m4a
+/home/beardydev/Music/FloFilz/Setagaya Samba/01 Setagaya Samba.m4a
+/home/beardydev/Music/Flowzz/Ueno Park/01 Ueno Park.m4a
+/home/beardydev/Music/Flowzz/Voyagers/02 La Boca.m4a
+/home/beardydev/Music/frad/Apollo/01 Apollo.m4a
+/home/beardydev/Music/Gatz2Gatz/I Miss Snow/01 I Miss Snow.m4a
+/home/beardydev/Music/Gatz2Gatz/Costa de Oro/01 Costa de Oro.m4a
+/home/beardydev/Music/Gxldxn Fxnch/The Way/01 The Way.m4a
+/home/beardydev/Music/Gxldxn Fxnch/It Was All A Dream/01 It Was All A Dream.m4a
+/home/beardydev/Music/Hayne/Nights Without You/01 Nights Without You.m4a
+/home/beardydev/Music/Hayne/Night Shift/01 Night Shift.m4a
+/home/beardydev/Music/HM Surf/Jola Shores/01 Jola Shores.m4a
+/home/beardydev/Music/Hoffy Beats/Exotica/04 Coconut Tree.m4a
+/home/beardydev/Music/HoKø/Sunset Wish/01 Sunset Wish.m4a
+/home/beardydev/Music/HoKø/Bubble Dream/01 Bubble Dream.m4a
+/home/beardydev/Music/Hoogway/High Ways/01 High Ways.m4a
+/home/beardydev/Music/Hoogway/Rejoice/01 Rejoice.m4a
+/home/beardydev/Music/Hoogway/Komorebi/01 Komorebi.m4a
+/home/beardydev/Music/Isaac/La Grande Jatte/01 La Grande Jatte.m4a
+/home/beardydev/Music/jaackson/have you_/01 have you_.m4a
+/home/beardydev/Music/jaackson/what is it i desire_/04 let's keep moving.m4a
+/home/beardydev/Music/Jam'addict/Lowkey/01 Lowkey.m4a
+/home/beardydev/Music/Javriii/Avatars Love javriii-mix/01 Avatars Love javriii-mix.m4a
+/home/beardydev/Music/JazzyHan/The Change of Seasons/01 The Change of Seasons.m4a
+/home/beardydev/Music/Juliàn/Fresh Air/01 Fresh Air.m4a
+/home/beardydev/Music/Juliàn/Volatile/01 Volatile.m4a
+/home/beardydev/Music/kanves/Loose Waves/01 Loose Waves.m4a
+/home/beardydev/Music/Kristoffer Eikrem/New Chapter/01 New Chapter.m4a
+/home/beardydev/Music/Kristoffer Eikrem/Jeju/01 Jeju.m4a
+/home/beardydev/Music/Kuba/Our Last Memories/01 Our Last Memories.m4a
+/home/beardydev/Music/Lazy Vibes/Let it Fall/01 Let it Fall.m4a
+/home/beardydev/Music/L.Dre/Korra/01 Korra.m4a
+/home/beardydev/Music/L.Dre/Secret Tunnel/01 Secret Tunnel.m4a
+/home/beardydev/Music/Leaflet/Secret Tunnel/01 Secret Tunnel.m4a
+/home/beardydev/Music/liid/Quiet Fields/01 Quiet Fields.m4a
+/home/beardydev/Music/L'Indécis/Goofy/01 Goofy.m4a
+/home/beardydev/Music/L'Indécis/Easy Going/01 Easy Going.m4a
+/home/beardydev/Music/Linn Mori/Uluwatu/02 Children of the Earth.m4a
+/home/beardydev/Music/Lofi Dreams/Minecraft Lofi/03 Minecraft Lofi Edit.m4a
+/home/beardydev/Music/Lotus Beats/Cloud Nine/01 Cloud Nine.m4a
+/home/beardydev/Music/Lotus Beats/Ocean View/01 Ocean View.m4a
+/home/beardydev/Music/Lotus Beats/By the Campfire/01 By the Campfire.m4a
+/home/beardydev/Music/Lotus Beats/Starry Night/01 Starry Night.m4a
+/home/beardydev/Music/Lotus Beats/Rise and Shine/01 Rise and Shine.m4a
+/home/beardydev/Music/Lotus Beats/Rising Sun/01 Rising Sun.m4a
+/home/beardydev/Music/Luv.Ly/Jazz Crusaders Beattape/08 Safe Return.m4a
+/home/beardydev/Music/luv pug/Shallow Oasis/01 Shallow Oasis.m4a
+/home/beardydev/Music/Makzo/Sometimes _ Why Not Go/02 Why Not Go.m4a
+/home/beardydev/Music/Makzo/Eden/01 Eden.m4a
+/home/beardydev/Music/Marcus D/Kodama/01 Kodama.m4a
+/home/beardydev/Music/Mayaewk/Golden Boy/01 Morning Lights.m4a
+/home/beardydev/Music/Mayaewk/Lonely Man/01 Lonely Man.m4a
+/home/beardydev/Music/Mayaewk/Tide/01 Tide.m4a
+/home/beardydev/Music/mell-ø/Beneath the Trees/01 Beneath the Trees.m4a
+/home/beardydev/Music/midnight alpha/Cascade/01 Cascade.m4a
+/home/beardydev/Music/Mik/The Force Theme (Binary Sunset) - Star Wars Lofi/01 The Force Theme (Binary Sunset) - Star Wars Lofi.m4a
+/home/beardydev/Music/mono._/Yearning/01 Yearning.m4a
+/home/beardydev/Music/morningtime/Noon Haze/01 Noon Haze.m4a
+/home/beardydev/Music/morningtime/come and go/01 come and go.m4a
+/home/beardydev/Music/Nat Walker/Inside/01 Inside.m4a
+/home/beardydev/Music/Nat Walker/Dawn Awaits/01 Dawn Awaits.m4a
+/home/beardydev/Music/Nat Walker/Air and Shape/01 Air and Shape.m4a
+/home/beardydev/Music/Nat Walker/Blue Hours/01 Blue Hours.m4a
+/home/beardydev/Music/Nat Walker/Rosario/01 Rosario.m4a
+/home/beardydev/Music/Nogymx/Sail Away/01 Sail Away.m4a
+/home/beardydev/Music/Nowun/Lumen/01 Lumen.m4a
+/home/beardydev/Music/Nujabes/samurai champloo music record departure/03 aruarian dance.m4a
+/home/beardydev/Music/Nujabes/Spiritual State/04 Spiral.m4a
+/home/beardydev/Music/Nuncc/Grooves/01 Grooves.m4a
+/home/beardydev/Music/Nymano/Vanilla Candle/01 Vanilla Candle.m4a
+/home/beardydev/Music/ØDYSSEE/Jade/01 Jade.m4a
+/home/beardydev/Music/ØDYSSEE/Markwood/01 Markwood.m4a
+/home/beardydev/Music/ØDYSSEE/Jetlag/01 Jetlag.m4a
+/home/beardydev/Music/ØDYSSEE/Aoba/01 Aoba.m4a
+/home/beardydev/Music/ØDYSSEE/Profondeurs/01 Profondeurs.m4a
+/home/beardydev/Music/Oliver Crosby/Too Soon/01 Too Soon.m4a
+/home/beardydev/Music/Pevanni/Headspace/01 Headspace.m4a
+/home/beardydev/Music/Redmatic レド/High Spirits/01 High Spirits.m4a
+/home/beardydev/Music/Retro Jungle/A Day In Japan/02 Blue Curry.m4a
+/home/beardydev/Music/Retro Jungle/A Day In Japan/05 Skylines.m4a
+/home/beardydev/Music/Retro Jungle/A Day In Japan/08 Mejiro Garden.m4a
+/home/beardydev/Music/Retro Jungle/A Day In Japan/09 Night City.m4a
+/home/beardydev/Music/Retro Jungle/A Day In Japan/16 Misplaced.m4a
+/home/beardydev/Music/Sachiho/Homewards/01 After The Rain.m4a
+/home/beardydev/Music/Saib/Entre Nous/01 Entre Nous.m4a
+/home/beardydev/Music/Sam Cross/Full Moon/01 Full Moon.m4a
+/home/beardydev/Music/Samuel Kim/Avatar_ The Last Airbender (Lo-Fi Collection)/03 Leaves from the Vine - Lofi Version.m4a
+/home/beardydev/Music/Screen Jazzmaster/Night City/01 Night City.m4a
+/home/beardydev/Music/S I M/_S W E E T .S O U _/01 _S W E E T .S O U _.m4a
+/home/beardydev/Music/Simon Groß/The Last Airbender (Lofi Mix)/01 The Peace Within.m4a
+/home/beardydev/Music/softy/Peace at Night/01 Peace at Night.m4a
+/home/beardydev/Music/softy/Unconscious Life/07 Unconscious Life.m4a
+/home/beardydev/Music/softy/Aura/01 Aura.m4a
+/home/beardydev/Music/someday/Fading/01 Fading.m4a
+/home/beardydev/Music/Spellspellspell/Kia Ora 10/01 Kia Ora.m4a
+/home/beardydev/Music/steezy prime/My Anchor/01 My Anchor.m4a
+/home/beardydev/Music/steezy prime/Starcrossed/01 Norway.m4a
+/home/beardydev/Music/steezy prime/Starcrossed/04 Let's go home.m4a
+/home/beardydev/Music/sunday museum/sway/01 sway.m4a
+/home/beardydev/Music/Swink/On The Road/03 By The Window.m4a
+/home/beardydev/Music/Swink/Night Connection/01 Night Connection.m4a
+/home/beardydev/Music/Swink/After so Long/01 After so Long.m4a
+/home/beardydev/Music/Swink/Live with Fear/01 Live with Fear.m4a
+/home/beardydev/Music/Swink/Inner Conflict/04 Need to Change.m4a
+/home/beardydev/Music/TABAL/Deep Sleep/01 Deep Sleep.m4a
+/home/beardydev/Music/TABAL/Lazy with You/01 Lazy with You.m4a
+/home/beardydev/Music/Tah/Rainforest/01 Rainforest.m4a
+/home/beardydev/Music/Tah/Wednesday Afternoon/01 Wednesday Afternoon.m4a
+/home/beardydev/Music/Tah/Without You/01 Without You.m4a
+/home/beardydev/Music/Tah/It's Getting Late/01 It's Getting Late.m4a
+/home/beardydev/Music/Tah/Feathers/01 It's Getting Late.m4a
+/home/beardydev/Music/Tah/Feathers/05 I Will Be Far Away.m4a
+/home/beardydev/Music/Tohaj/Guava/01 Guava.m4a
+/home/beardydev/Music/Tune in with Chewie/Gate of Time (The Legend of Zelda_ Skyward Sword)/01 Gate of Time (The Legend of Zelda_ Skyward Sword).m4a
+/home/beardydev/Music/Twill Street/Archwood/01 Archwood.m4a
+/home/beardydev/Music/Twill Street/Coastal Haven/02 Bayside.m4a
+/home/beardydev/Music/Twill Street/Coastal Haven/03 Blue Harbor.m4a
+/home/beardydev/Music/Twill Street/Coastal Haven/04 Mirage.m4a
+/home/beardydev/Music/Unwind Project/Heading to Dinner/01 Heading to Dinner.m4a
+/home/beardydev/Music/Various Artists/THE HEIST/12 Jumbled.m4a
+/home/beardydev/Music/Various Artists/Summer Memories/01 Painted the Night.m4a
+/home/beardydev/Music/Various Artists/Summer Memories/06 Delicate.m4a
+/home/beardydev/Music/Various Artists/Summer Memories/09 Let Go.m4a
+/home/beardydev/Music/Various Artists/Summer Memories/10 Ten Days.m4a
+/home/beardydev/Music/Various Artists/Winter Haze/04 On my Mind.m4a
+/home/beardydev/Music/Various Artists/Winter Haze/11 Snowball Effect.m4a
+/home/beardydev/Music/Various Artists/Lost Paradise/11 Final Lap.m4a
+/home/beardydev/Music/Various Artists/The Lyrical Mansion/08 High Towers.m4a
+/home/beardydev/Music/Various Artists/Summer Breeze/03 The Waters.m4a
+/home/beardydev/Music/Various Artists/Summer Breeze/05 Moonlight Drift.m4a
+/home/beardydev/Music/Various Artists/Summer Breeze/14 Gate A.m4a
+/home/beardydev/Music/Various Artists/Summer Breeze/15 Golf 'n stuff.m4a
+/home/beardydev/Music/Various Artists/Echoes from Hakone/08 Hiace Groove.m4a
+/home/beardydev/Music/Various Artists/Echoes from Hakone/13 Riverside.m4a
+/home/beardydev/Music/WYS/Yosemite/07 Summer 89.m4a
+/home/beardydev/Music/yungmerrin/Jazz Class/01 Jazz Class.m4a
+/home/beardydev/Music/yungmerrin/liquorice/01 liquorice.m4a
+/home/beardydev/Music/yungmerrin/Dahlia/01 Dahlia.m4a
+/home/beardydev/Music/yungmerrin/Wish I knew/01 Wish I knew.m4a
